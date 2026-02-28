dc8c768aaa43fde4baea5abce5f2c8c3d985b734
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 10:09:31 +0800

    fix(util): 优化 addSearchParams 函数实现及补全测试

--- src/add-search-params.js
+++ src/add-search-params.js
@@ -30,26 +30,45 @@ import getHash from './get-hash';
  *     regularized.
  * @author Haixing Hu
  */
+/**
+ * Add a parameter to the query string of the current address. Note that the
+ * added parameter will be URL encoded.
+ *
+ * Because the addresses of Vue.js are all in hash form for routing, the basic
+ * `window.location.search` cannot be used to obtain the correct query string.
+ * For example: `http://dev.example.com/test/#/finish?params=xxx`.
+ *
+ * @param {Object} params
+ *     Each attribute name and attribute value of this argument will be added
+ *     to the query string of the current address as a key-value pair.
+ * @param {String|URL} url
+ *     An optional argument, indicating the URL address to be parsed, as a
+ *     `String` or `URL` object. If this argument is missing, `window.location`
+ *     is used.
+ * @returns {String}
+ *     A new link address with the specified parameters added and properly
+ *     regularized.
+ * @author Haixing Hu
+ */
 function addSearchParams(params, url) {
+  let theUrl;
   if (url === undefined) {
-    url = window.location;
-  } else if (!(url instanceof URL)) {
+    theUrl = window.location;
+  } else if (url instanceof URL) {
+    theUrl = url;
+  } else {
     try {
-      url = new URL(url);
+      theUrl = new URL(url);
     } catch (_e) {
       // if the URL is invalid, fall back to window.location
-      url = window.location;
+      theUrl = window.location;
     }
   }
-  const base = url.origin + url.pathname;
-  let hash = getHash(url);
-  if (hash == null) {
-    hash = '';
-  } else {
-    hash = `#${hash}`;
-  }
+  const base = theUrl.origin + theUrl.pathname;
+  const hashPart = getHash(theUrl);
+  const hash = (hashPart === null ? '' : `#${hashPart}`);
   const kv = queryString.stringify(params);
-  let search = getSearch(url);
+  let search = getSearch(theUrl);
   if (search !== null && search.length > 0) {
     search = `${search}&${kv}`;
   } else {
